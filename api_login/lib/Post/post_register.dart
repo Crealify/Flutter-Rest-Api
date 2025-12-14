421d7cccf63f09465cca652a6f0cbd59af7a289a
Anil Bhattarai <bhattaraianil2015@gmail.com>
2025-12-14 18:05:54 +0545

    RegisterUser added

---
+++ api_login/lib/Post/post_register.dart
@@ -0,0 +1,86 @@
+// ignore_for_file: avoid_print
+
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class PostRegister extends StatefulWidget {
+  const PostRegister({super.key});
+
+  @override
+  State<PostRegister> createState() => _PostRegisterState();
+}
+
+class _PostRegisterState extends State<PostRegister> {
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  registerUser(String email, String password) async {
+    Uri url = Uri.parse("https://reqres.in/api/register");
+    var data = {"email": email, "password": password};
+
+    try {
+      var response = await http.post(url, body: data);
+      if (response.statusCode == 200) {
+        var jsonData = jsonDecode(response.body);
+        print(jsonData);
+      } else {
+        var error = jsonDecode(response.body);
+        print("Unable to Register: ${error['error']}");
+      }
+    } catch (e) {
+      print("Errpr: $e");
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Register Post Api"), centerTitle: true),
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextFormField(
+              controller: emailController,
+              decoration: const InputDecoration(hintText: "Email"),
+            ),
+            const SizedBox(height: 20),
+            TextFormField(
+              controller: passwordController,
+              decoration: const InputDecoration(hintText: "Password"),
+            ),
+
+            const SizedBox(height: 30),
+            GestureDetector(
+              onTap: () {
+                registerUser(
+                  emailController.text.toString(),
+                  passwordController.text.toString(),
+                );
+              },
+              child: Container(
+                height: 50,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  color: Colors.blue,
+                ),
+                child: Center(
+                  child: Text(
+                    "Register",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
