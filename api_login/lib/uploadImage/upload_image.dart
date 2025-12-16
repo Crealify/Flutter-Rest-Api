85371e40e7c2e769dea90f1a929479e8ae17bced
Anil Bhattarai <bhattaraianil2015@gmail.com>
2025-12-16 12:18:23 +0545

    finally Upload Image Sucessfully

--- api_login/lib/uploadImage/upload_image.dart
+++ api_login/lib/uploadImage/upload_image.dart
@@ -13,59 +13,76 @@ class UploadImage extends StatefulWidget {
 
 class _UploadImageState extends State<UploadImage> {
   @override
-  String isImageUploade = '';
+  String isImageUpload = '';
+  bool isLoading = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.blueAccent,
-              ),
-              onPressed: () async {
-                final ImagePicker picker = ImagePicker();
-                final XFile? image = await picker.pickImage(
-                  source: ImageSource.gallery,
-                );
-                if (image != null) {
-                  Uint8List bytes = await image.readAsBytes();
-                  UploadApiImage()
-                      .uploadImage(bytes, image.name)
-                      .then((value) {
+        child: isLoading
+            ? CircularProgressIndicator()
+            : Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  isImageUpload == ""
+                      ? SizedBox()
+                      : SizedBox(
+                          height: 350,
+                          width: 350,
+                          child: Image.network(isImageUpload),
+                        ),
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.blueAccent,
+                    ),
+                    onPressed: () async {
+                      final ImagePicker picker = ImagePicker();
+                      final XFile? image = await picker.pickImage(
+                        source: ImageSource.gallery,
+                      );
+                      if (image != null) {
                         setState(() {
-                          isImageUploade = value['location'].toString();
+                          isLoading = true;
                         });
-                        print(
-                          "Upload Sucessfully with link ${value.toString()}",
-                        );
-                      })
-                      .onError((error, stackTrace) {
-                        print(error.toString());
-                      });
-                }
-              },
+                        Uint8List bytes = await image.readAsBytes();
+                        UploadApiImage()
+                            .uploadImage(bytes, image.name)
+                            .then((value) {
+                              setState(() {
+                                isImageUpload = value['location'].toString();
+                                isLoading = false;
+                              });
+                              print(
+                                "Upload Sucessfully with link ${value.toString()}",
+                              );
+                            })
+                            .onError((error, stackTrace) {
+                              setState(() {
+                                isLoading = true;
+                              });
+                              print(error.toString());
+                            });
+                      }
+                    },
 
-              // onPressed: () async {
-              //   final picker = ImagePicker();
-              //   final image = await picker.pickImage(
-              //     source: ImageSource.gallery,
-              //   );
-              //   print(image?.path);
-              // },
-              child: const Text(
-                "Upload Image",
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 18,
-                  color: Colors.white,
-                ),
+                    // onPressed: () async {
+                    //   final picker = ImagePicker();
+                    //   final image = await picker.pickImage(
+                    //     source: ImageSource.gallery,
+                    //   );
+                    //   print(image?.path);
+                    // },
+                    child: const Text(
+                      "Upload Image",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ],
               ),
-            ),
-          ],
-        ),
       ),
     );
   }
