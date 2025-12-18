7e2ed52494c1746f1855822804b186a8b7d29f47
Anil Bhattarai <bhattaraianil2015@gmail.com>
2025-12-18 16:34:32 +0545

    Appling padding for ui

--- news_app_restapi/lib/screens/news_details.dart
+++ news_app_restapi/lib/screens/news_details.dart
@@ -5,30 +5,34 @@ import 'package:news_app_restapi/model/news_model.dart';
 class NewsDetails extends StatelessWidget {
   final NewsModel newsModel;
   const NewsDetails({super.key, required this.newsModel});
-
+ 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: ListView(
-        children: [
-          Text(
-            newsModel.title!,
-            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
-          ),
-          Row(
-            children: [
-              Expanded(child: SizedBox()),
-              Spacer(),
-              Expanded(child: Text("- ${newsModel.author!}", maxLines: 1)),
-            ],
-          ),
-          SizedBox(height: 10),
-          Image.network(newsModel.urlToImage!),
-          SizedBox(height: 10),
-          Text(newsModel.content!, style: TextStyle(fontSize: 18)),
-          Text(newsModel.description!, style: TextStyle(fontSize: 18)),
-        ],
+      body: Padding(
+        padding: const EdgeInsets.only(right: 8.0, left: 10),
+        child: ListView(
+          children: [
+            Text(
+              newsModel.title!,
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 22),
+            ),
+            Row(
+              children: [
+                Expanded(child: SizedBox()),
+                Spacer(),
+                Expanded(child: Text("- ${newsModel.author!}", maxLines: 1)),
+              ],
+            ),
+            SizedBox(height: 10),
+            Image.network(newsModel.urlToImage!),
+            SizedBox(height: 10),
+            Text(newsModel.content!, style: TextStyle(fontSize: 18)),
+            SizedBox(height: 10),
+            Text(newsModel.description!, style: TextStyle(fontSize: 15)),
+          ],
+        ),
       ),
     );
   }
