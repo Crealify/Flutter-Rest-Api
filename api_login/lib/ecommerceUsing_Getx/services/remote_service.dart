accabd953542349acde3456dbebca443f1401db1
Anil Bhattarai <bhattaraianil2015@gmail.com>
2025-12-19 09:53:06 +0545

    model and Remoteservice integreated

---
+++ api_login/lib/ecommerceUsing_Getx/services/remote_service.dart
@@ -0,0 +1,21 @@
+//fakestoreapi.com
+
+import 'package:http/http.dart' as http;
+
+import '../model/model.dart';
+
+class RemoteService {
+  Future<List<Product>?> fetchProduct() async {
+    var client = http.Client();
+
+    var uri = Uri.parse("https://fakestoreapi.com/products");
+    var response = await client.get(uri);
+    if (response.statusCode == 200) {
+      var json = response.body;
+      return productFromJson(json);
+    } else {
+      print("Error Response : ${response.body}");
+    }
+    return null;
+  }
+}
